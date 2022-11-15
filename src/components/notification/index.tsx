96c4b2b2831ab91e9a44cc21aee64c0a6263f628
fredy bustos <afredybustos@gmail.com>
2022-11-14 23:08:54 -0500

    updated props

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { createPortal } from 'react-dom'
 import { clsx } from 'clsx'
 
 import CloseIcon from 'components/closeIcon'
-import useUtils from 'hooks/useUtils'
-import { TYPE, DEFAULT_POSITION, POSITION } from 'styles/utils'
-import { NotifyProps } from 'types'
+import { DEFAULT_POSITION, POSITION, TYPE } from 'styles/utils'
+import { NotificationProps } from 'types'
+import useToast from 'hooks/useToast'
 import '../../styles/notify.css'
 
 export default function Notification({
-  message,
+  id,
+  open,
   type,
-  openNotify,
-  options
-}: NotifyProps) {
-  const [open, setOpen] = useState(false)
-
-  const { container } = useUtils({ open, options, setOpen })
-
-  useEffect(() => {
-    setOpen(openNotify)
-  }, [openNotify])
-
-  const closeNotify = (event: React.MouseEvent<SVGSVGElement>) => {
-    if (options?.onClose) {
-      options.onClose(event)
-      setOpen(false)
-      return
-    }
-    setOpen(false)
-  }
+  options,
+  message,
+  container,
+  setOpen
+}: NotificationProps) {
+  const { animatedStyles, closeNotify } = useToast({
+    setOpen,
+    options,
+    container,
+    id,
+    open
+  })
 
   return createPortal(
     <>
       {options?.component ? (
-        <div className="notify-container" role="button">
-          {options?.component({ message, type })}
+        <div
+          style={{ ...animatedStyles, background: options?.bgcolor }}
+          className={clsx([
+            'notify-container',
+            { [POSITION[options?.position || 'top']]: open },
+            {
+              [DEFAULT_POSITION[options?.position || 'top']]: !open
+            }
+          ])}
+        >
+          {options?.component({ message, type, options })}
         </div>
       ) : (
         <div
-          role="button"
+          style={{ ...animatedStyles, background: options?.bgcolor }}
           className={clsx([
             'notify-container',
             `${TYPE[type]}`,
@@ -50,7 +53,9 @@ export default function Notification({
           ])}
         >
           <div className="notify-body">
-            <p className="notify-title">{message}</p>
+            <p className="notify-title" style={{ color: options?.color }}>
+              {message}
+            </p>
             <CloseIcon onClick={closeNotify} />
           </div>
         </div>
