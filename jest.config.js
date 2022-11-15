293e4e46a2b8b7287f5eaaf53c9d06f46efb0064
fredy bustos <afredybustos@gmail.com>
2022-11-15 00:14:19 -0500

    updated test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '.(css|less|scss|sass)$': 'identity-obj-proxy'
   }
--- src/__test__/Notify.test.tsx
+++ src/__test__/Notify.test.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import { NotifyProps } from 'types'
-import Notify from 'components/notification'
+import { Notify } from 'components/notify'
 
 const props: NotifyProps = {
-  message: 'Notification',
   type: 'success',
-  openNotify: true,
+  message: 'Notification',
   options: {
-    duration: 5000
+    duration: 5000,
+    position: 'top'
   }
 }
 
